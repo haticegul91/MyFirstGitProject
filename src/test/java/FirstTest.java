4d51f077022126740df6c80ae78fdcc53958697e
Hatice Dagdelen <hatice_gul_dalbudak@hotmail.com>
2019-07-30 18:14:41 -0700

    first commit

--- src/test/java/FirstTest.java
+++ src/test/java/FirstTest.java
@@ -2,7 +2,7 @@ public class FirstTest {
     public static void main(String[] args) {
         System.out.println("Hello World");
         git init
-         git add .
+        git add .
         git commit -m "first commit"
         git remote add origin https:https://github.com/haticegul91
         git push -u origin master
